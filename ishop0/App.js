9e6137bd1809b551b56ac3ab20cd1f20dfa31326
Viktoriya Kovalenko <viktoriya.kovalenko21@bk.ru>
2025-10-22 15:40:04 +0300

    Интернет-магазин

---
+++ ishop0/App.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(
+  <Shop 
+  name="Интернет-магазин тканей" 
+  address ="Минск, пр. Независимости 134"/>,
+  document.getElementById('container') 
+);
---
+++ ishop0/components/Shop.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className='Shop'>
+        <p className='Shop_name'>
+          {this.props.name}
+        </p>
+        <p className='Shop_address'>
+          {this.props.address}
+        </p>
+      </div>
+    );
+  }
+
+}
+
+export default Shop;
