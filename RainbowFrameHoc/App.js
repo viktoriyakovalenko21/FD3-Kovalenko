8343e9e536a677c546367233013cf3680d2a5e20
Viktoriya Kovalenko <viktoriya.kovalenko21@bk.ru>
2025-11-28 21:39:31 +0300

    Rainbow Frame Hoc

---
+++ RainbowFrameHoc/App.js
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DoubleButton from './components/DoubleButton';
+import withRainbowFrame from './components/withRainbowFrame';
+
+class App extends React.Component {
+  
+  handlePressed = (num) => {
+    alert(num);
+  }
+
+  render() {
+    let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+
+    let FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
+
+    return (
+      <div style={{ padding: '20px' }}>
+      
+        <div style={{ marginBottom: '30px' }}>
+          <DoubleButton 
+            caption1="однажды" 
+            caption2="пору" 
+            cbPressed={this.handlePressed}
+          >
+            в студеную зимнюю
+          </DoubleButton>
+        </div>
+
+        <div>
+          <FramedDoubleButton 
+            caption1="я из лесу" 
+            caption2="мороз" 
+            cbPressed={this.handlePressed}
+          >
+            вышел, был сильный
+          </FramedDoubleButton>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
